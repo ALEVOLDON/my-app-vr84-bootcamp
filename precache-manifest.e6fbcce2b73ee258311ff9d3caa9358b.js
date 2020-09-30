f9210cacacb6641eb01d0fe0d3fda33c4ba0f010
Vladimir Rybalsky <alevoldon@gmail.com>
2020-09-30 10:05:10 +0300

    Updates

---
+++ precache-manifest.e6fbcce2b73ee258311ff9d3caa9358b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24deb2b4301277f696066b5209ab3366",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/index.html"
+  },
+  {
+    "revision": "6a954f301475ef913074",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/css/2.e0686c58.chunk.css"
+  },
+  {
+    "revision": "3c3c0464181499d8e079",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/css/main.c886b8f7.chunk.css"
+  },
+  {
+    "revision": "6a954f301475ef913074",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/js/2.dd8d3168.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/js/2.dd8d3168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3c0464181499d8e079",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/js/main.acf77c94.chunk.js"
+  },
+  {
+    "revision": "7287f0cde3a2276df40f",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/js/runtime-main.3e7949fe.js"
+  },
+  {
+    "revision": "e60be19e8cbfbd550ac9a3f905ad359a",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/media/GC_logo.e60be19e.svg"
+  },
+  {
+    "revision": "899cd2784349ab5b5a24f5dcee2ffca3",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/media/ITN_logo.899cd278.svg"
+  },
+  {
+    "revision": "f8f67844654cd9fc78dd57834ecfed6c",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/media/city_planing.f8f67844.jpg"
+  },
+  {
+    "revision": "43f9dac62ffb9209e823b91b7504c5ec",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/media/energetics.43f9dac6.jpg"
+  },
+  {
+    "revision": "fb084e1e416e340515649cb48eb04f6e",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/media/energy.fb084e1e.jpg"
+  },
+  {
+    "revision": "5d86912d3747f4d2fb2419bd7d2de74b",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/media/hero-bg.5d86912d.jpg"
+  },
+  {
+    "revision": "5ec44869469e2f3c9db3551c44662f88",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/media/industry.5ec44869.jpg"
+  },
+  {
+    "revision": "8dd4ef7149a55ade70696aa0e9e2faa3",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/media/shid_emozhl_logo.8dd4ef71.svg"
+  },
+  {
+    "revision": "7e1db0da3d25263be6534e000b29d2a8",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/media/slide-1.7e1db0da.jpg"
+  },
+  {
+    "revision": "12a25e248f105a327021b69336c5b71b",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/media/slide-2.12a25e24.jpg"
+  },
+  {
+    "revision": "638ec2275a294137e028e6bce58ad768",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/media/slide-3.638ec227.jpg"
+  },
+  {
+    "revision": "8a21e8b7fc0908ab482cc0bb0c98138a",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/media/slide-4.8a21e8b7.jpg"
+  },
+  {
+    "revision": "bb9f49bba9c380e18e77de4c1ed82bf6",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/media/slide-5.bb9f49bb.jpg"
+  },
+  {
+    "revision": "65643b05446e10934c8778999860e9de",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/media/transport.65643b05.jpg"
+  },
+  {
+    "revision": "a8b6caeec11d336276f5ed05a63fc938",
+    "url": "/G-T-W-Y/Ambient-air-quality-and-health/static/media/utilisation.a8b6caee.jpg"
+  }
+]);
